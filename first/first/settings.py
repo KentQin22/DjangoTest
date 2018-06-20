63d7b48ccf1d1e6d45c4153f3da052b18b3b55d1
Qinhao <qinhaohao163@163.com>
2018-06-20 17:22:15 +0800

    更改了settings中的时区与语言设置

--- first/first/settings.py
+++ first/first/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h_zvxksne-hn(1-eu%1ry-q44x9h)18csbfo0sqfk7ouaa-ou-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
